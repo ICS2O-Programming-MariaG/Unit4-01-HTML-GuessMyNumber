9957b11d65d1f162d469b3cdc9946b59bca49f3e
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-04-25 15:05:46 +0000

    Added if/then statement - button not working

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Copyright (c) 2022 Maria Goemans All rights reserved
+//
+// Created by: Maria G
+// Created on: April 25, 2022
+// This file contains the JS functions for index.html
+
+function guessSent() {
+  const MAX = 6;
+  const MIN = 1;
+  let correctNumber = Math.floor(Math.random() * MAX + MIN);
+  let guessedNumber = document.getElementById("user-guess").value;
+  //if/then statement: if correctNumber is equal to guessedNumber
+  if (correctNumber == guessedNumber) {
+    document.getElementById("results").innerHTML = "You are correct!";
+  }
+  //else: if the numbers are not equal
+  else {
+    document.getElementById("results").innerHTML = "Your guess is incorrect! The right number was " + correctNumber + ".";
+  }
+}
